a2534f85bad9d865c9172d6f89b6cada76632b75
Dimitar-Peev <mctrix87@abv.bg>
2026-03-29 20:24:10 +0300

    Fix Terraform formatting

--- terraform-backend/main.tf
+++ terraform-backend/main.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
 
-   backend "azurerm" {
+  backend "azurerm" {
     resource_group_name  = "terraform-state-rg"
     storage_account_name = "tfstate10494"
     container_name       = "tfstate"
