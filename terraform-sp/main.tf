25811cc10079803e80f7b7f4cebe4c54875d0c64
Dimitar-Peev <mctrix87@abv.bg>
2026-03-29 20:42:03 +0300

    Update terraform-sp to use data sources instead of creating SP

--- terraform-sp/main.tf
+++ terraform-sp/main.tf
@@ -18,35 +18,20 @@ provider "azurerm" {
 
 provider "azuread" {}
 
-data "azurerm_subscription" "current" {}
-
-resource "azuread_application" "app" {
+# Чете съществуващия SP без да го създава
+data "azuread_application" "app" {
   display_name = var.sp_name
 }
 
-resource "azuread_service_principal" "sp" {
-  client_id = azuread_application.app.client_id
+data "azuread_service_principal" "sp" {
+  client_id = data.azuread_application.app.client_id
 }
 
-resource "azuread_service_principal_password" "sp_secret" {
-  service_principal_id = azuread_service_principal.sp.id
-}
-
-resource "azurerm_role_assignment" "contributor" {
-  scope                = data.azurerm_subscription.current.id
-  role_definition_name = "Contributor"
-  principal_id         = azuread_service_principal.sp.id
-}
+data "azurerm_subscription" "current" {}
 
 output "client_id" {
-  value       = azuread_application.app.client_id
+  value       = data.azuread_application.app.client_id
   description = "Client ID на Service Principal"
-}
-
-output "client_secret" {
-  value       = azuread_service_principal_password.sp_secret.value
-  sensitive   = true
-  description = "Client Secret на Service Principal"
 }
 
 output "tenant_id" {
@@ -57,4 +42,9 @@ output "tenant_id" {
 output "subscription_id" {
   value       = data.azurerm_subscription.current.subscription_id
   description = "Subscription ID"
+}
+
+output "sp_object_id" {
+  value       = data.azuread_service_principal.sp.object_id
+  description = "Object ID на Service Principal"
 }
