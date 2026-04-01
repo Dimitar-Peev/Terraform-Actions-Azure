4f7e33f9af5c55c668c198f5208477e5a63af31c
Dimitar-Peev <mctrix87@abv.bg>
2026-04-01 20:30:19 +0300

    Add Azure AD provider and resources; update backend configuration

--- terraform-sp/variables.tf
+++ terraform-sp/variables.tf
@@ -2,8 +2,3 @@ variable "subscription_id" {
   type        = string
   description = "Azure Subscription ID"
 }
-
-variable "client_id" {
-  type        = string
-  description = "Client ID на съществуващия Service Principal"
-}
