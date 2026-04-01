388b747a2e22b2b4851f17971d02bf262c88a8b8
Dimitar-Peev <mctrix87@abv.bg>
2026-04-01 20:49:14 +0300

    Remove Azure AD resources and outputs; update variable definitions for client ID

--- terraform-sp/main.tf
+++ terraform-sp/main.tf
@@ -4,17 +4,6 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "4.66.0"
     }
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "~> 3.0"
-    }
-  }
-
-  backend "azurerm" {
-    resource_group_name  = "terraform-state-rg"
-    storage_account_name = "tfstate10494"
-    container_name       = "tfstate"
-    key                  = "terraform-sp.tfstate"
   }
 }
 
@@ -22,37 +11,19 @@ provider "azurerm" {
   features {}
 }
 
-provider "azuread" {}
-
-resource "azuread_application" "taskboard_app" {
-  display_name = "taskboard-service-principal"
-}
-
-resource "azuread_service_principal" "sp" {
-  client_id = azuread_application.taskboard_app.client_id
-}
-
-resource "azuread_service_principal_password" "sp_pwd" {
-  service_principal_id = azuread_service_principal.sp.id
-}
-
 data "azurerm_subscription" "current" {}
 
-resource "azurerm_role_assignment" "sp_role" {
-  scope                = data.azurerm_subscription.current.id
-  role_definition_name = "Contributor"
-  principal_id         = azuread_service_principal.sp.object_id
+output "client_id" {
+  value       = var.client_id
+  description = "Client ID на Service Principal"
 }
 
-output "new_client_id" {
-  value = azuread_application.taskboard_app.client_id
+output "tenant_id" {
+  value       = data.azurerm_subscription.current.tenant_id
+  description = "Tenant ID"
 }
 
-output "new_client_secret" {
-  value     = azuread_service_principal_password.sp_pwd.value
-  sensitive = true
-}
-
-output "new_tenant_id" {
-  value = data.azurerm_subscription.current.tenant_id
+output "subscription_id" {
+  value       = data.azurerm_subscription.current.subscription_id
+  description = "Subscription ID"
 }
--- terraform-sp/variables.tf
+++ terraform-sp/variables.tf
@@ -2,3 +2,8 @@ variable "subscription_id" {
   type        = string
   description = "Azure Subscription ID"
 }
+
+variable "client_id" {
+  type        = string
+  description = "Client ID на съществуващия Service Principal"
+}
